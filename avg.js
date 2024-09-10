f5e4d7cf28e64f7addb4959bd711971874e836ac
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-09-11 01:19:20 +0600

    all complate

---
+++ avg.js
@@ -0,0 +1,25 @@
+function waitingTime(waitingTimes, serialNumber) {
+  if (!Array.isArray(waitingTimes) || typeof serialNumber !== "number") {
+    return "Invalid Input";
+  }
+
+  let totalTime = 0;
+  for (const time of waitingTimes) {
+    totalTime += time;
+  }
+
+  const averageTime = Math.round(totalTime / waitingTimes.length);
+  const peopleAhead = serialNumber - 1;
+  const peoples = peopleAhead - waitingTimes.length;
+  const waitingTime = averageTime * peoples;
+
+  return waitingTime;
+}
+
+console.log(waitingTime([3, 5, 7, 11, 6], 10));
+console.log(waitingTime([13, 2], 6));
+console.log(waitingTime([13, 2, 6, 7, 10], 6));
+console.log(waitingTime([6], 4));
+console.log(waitingTime(7 , 10));
+console.log(waitingTime("[6,2]", 9));
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));
