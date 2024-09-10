02cf5977d9731491a3bda859dbd86958641d5ff2
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-09-10 21:36:32 +0600

    tax calculation complate

--- tax.js
+++ tax.js
@@ -1,1 +1,19 @@
-console.log('foisal')
+function calculateTax(income, expenses) {
+  if (income < 0 || income < expenses || expenses < 0) {
+    return "Invalid Input";
+  }
+
+  if (income >= expenses) {
+    let profits = income - expenses;
+    let tax = profits * 0.2;
+    return tax;
+  }
+}
+
+console.log(calculateTax(10000, 3000));
+console.log(calculateTax(34000, 1753));
+console.log(calculateTax(5000, 1500));
+console.log(calculateTax(7000, 7000));
+console.log(calculateTax(-5000, 2000));
+console.log(calculateTax(6000, -1500));
+console.log(calculateTax(-6000, -1500));
